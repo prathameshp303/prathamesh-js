2bd09c53325268a4df38ec57ede9799a443a68f3
prathameshp303 <patankarprathamesh303@gmail.com>
2024-09-02 10:29:32 +0000

    control flow

---
+++ 02-controlflow/one.js
@@ -0,0 +1,39 @@
+
+
+// if (true){
+
+//     console.log("executed")
+// }
+
+// //comparision operators: <, >, <=, >=, ==, ===, !=
+
+// // === will also check datatye of the value unlike == operator
+
+// if (2 === "2"){
+
+//     console.log("executed")
+
+// }
+
+//=================================================================================================================================
+
+
+//scope of a varibale: if you use let or const to define a variable inside a scope{} 
+//it can't execute that vaiable if it executaed outside the scope
+
+// let temp = 34
+
+// if (temp > 30){
+//     let feel = "hot"
+//     console.log(`it is ${feel} today`);    
+    
+// }
+
+// console.log(`is it ${feel}`);
+
+//=================================================================================================================================
+
+// one line execution
+
+// if (true) console.log("execute");
+
